47fd699518fe1f8ec38238929ffd4d0710cac559
Mrshieh817 <18716398693@163.com>
2019-05-28 16:51:35 +0800

    token

--- src/main/java/com/soft/Controller/UserController.java
+++ src/main/java/com/soft/Controller/UserController.java
@@ -6,11 +6,8 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.soft.model.usermodel;
 import com.soft.tools.JwtUtils;
 
